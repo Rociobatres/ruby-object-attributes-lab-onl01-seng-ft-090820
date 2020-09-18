6aa79b30c43774de98c6c7e1e4d722696d77dfb2
Rocio Batres <jonasgirl21@gmail.com>
2020-09-18 00:51:53 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog 
+  def name=(dog_name)
+    @dog_name = name 
+  end 
+  def name 
+    @dog_name 
+  end 
+  def breed=(dog_breed)
+    @dog_breed = breed
+  end 
+  def breed 
+    @dog_breed 
+  end   
+end   
